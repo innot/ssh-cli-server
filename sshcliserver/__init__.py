c2a29cdbb2fb36cd84d08bcccee16fe68c57d085
Thomas Holland <thomas@innot.de>
2022-07-19 14:48:42 +0200

    Initial commit

---
+++ sshcliserver/__init__.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2022 Thomas Holland
+#
+#  This work is licensed under the terms of the MIT license.
+#  For a copy, see the accompanying LICENSE.txt file or
+#  go to <https://opensource.org/licenses/MIT>.
+#
+
+__version__ = 0.1.0
